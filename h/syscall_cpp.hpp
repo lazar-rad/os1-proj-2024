80c3493638ec0a30c33445b5d9fd272679285ec1
lazar-rad <lazar.n.rad@gmail.com>
2024-07-08 00:38:11 +0200

    README.md added

--- h/syscall_cpp.hpp
+++ h/syscall_cpp.hpp
@@ -21,20 +21,20 @@ public:
 
     static int sleep (time_t timeout) { return time_sleep(timeout); }
 
-    void send(const char* message) { ::send(myHandle, message); }
+    void send (const char* message) { ::send(myHandle, message); }
 
-    static const char* receive() { return ::receive(); }
+    static const char* receive () { return ::receive(); }
 
-    int timedJoin(time_t timeout) { return ::timedJoin(myHandle, timeout); }
+    int timedJoin (time_t timeout) { return ::timedJoin(myHandle, timeout); }
 
-    static void joinAll() { return thread_joinall(); }
+    static void joinAll () { return thread_joinall(); }
 
 protected:
     Thread () : myHandle(nullptr), body(nullptr), arg(nullptr) { }
     virtual void run () {}
 
 private:
-    static void runWrapper(void* p);
+    static void runWrapper (void* p);
 
     thread_t myHandle;
     void (*body)(void*);
@@ -49,7 +49,7 @@ public:
 
 protected:
     PeriodicThread (time_t period) : period(period) { }
-    virtual void run() override;
+    virtual void run () override;
     virtual void periodicActivation () {}
 
 private:
@@ -61,17 +61,17 @@ private:
 class Semaphore
 {
 public:
-    Semaphore(unsigned init = 1) { sem_open(&myHandle, init); }
+    Semaphore (unsigned init = 1) { sem_open(&myHandle, init); }
 
-    virtual ~Semaphore() { sem_close(myHandle); }
+    virtual ~Semaphore () { sem_close(myHandle); }
 
-    int wait()           { return sem_wait(myHandle); }
+    int wait ()           { return sem_wait(myHandle); }
 
-    int signal()         { return sem_signal(myHandle); }
+    int signal ()         { return sem_signal(myHandle); }
 
     int timedWait (time_t timeout) { return sem_timedwait(myHandle, timeout); }
 
-    int tryWait()        { return sem_trywait(myHandle); }
+    int tryWait ()        { return sem_trywait(myHandle); }
 
 private:
     sem_t myHandle;
